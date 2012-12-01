5185d2141e2b0c8940c3dca0197329bbd3a85a82
LymanEfunktion <lymanefunktion@gmail.com>
2012-12-01 23:17:54 +0100

    Splash und ProgressBar hinzugefuegt

--- de.rocovomo.osgi.e4.rcp/src/de/rocovomo/osgi/e4/rcp/Activator.java
+++ de.rocovomo.osgi.e4.rcp/src/de/rocovomo/osgi/e4/rcp/Activator.java
@@ -1,9 +1,14 @@
 package de.rocovomo.osgi.e4.rcp;
 
+import org.eclipse.core.runtime.IProduct;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IWorkbenchPreferenceConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.branding.IProductConstants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator{
 
 	private static BundleContext context;
 
@@ -16,6 +21,16 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
+//		PlatformUI.getPreferenceStore().putValue(IProductConstants.STARTUP_PROGRESS_RECT, "0,5,100,20");
+//		PlatformUI.getPreferenceStore().putValue(IProductConstants.STARTUP_FOREGROUND_COLOR, "124316");
+		PlatformUI.getPreferenceStore().setDefault(IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP,true);
+		String test = PlatformUI.getPreferenceStore().getString(IWorkbenchPreferenceConstants.SHOW_PROGRESS_ON_STARTUP);
+//		IProductConstants.STARTUP_PROGRESS_RECT
+		System.out.println(test);
+		IProduct product = Platform.getProduct();
+		System.out.println(product.getProperty(IProductConstants.STARTUP_FOREGROUND_COLOR));
+		System.out.println(product.getProperty(IProductConstants.STARTUP_PROGRESS_RECT));
+		System.out.println(product.getProperty(IProductConstants.STARTUP_MESSAGE_RECT));
 		Activator.context = bundleContext;
 	}
 
@@ -26,5 +41,4 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
-
 }

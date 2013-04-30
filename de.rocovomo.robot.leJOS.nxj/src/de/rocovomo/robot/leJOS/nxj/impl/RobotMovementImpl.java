54483b054fb68d6aae2decc814e8765e2fb3ef2b
Volker W <vowe91@googlemail.com>
2013-04-30 10:56:20 +0200

    leJOS NXJ Robot implementation bundle added

---
+++ de.rocovomo.robot.leJOS.nxj/src/de/rocovomo/robot/leJOS/nxj/impl/RobotMovementImpl.java
@@ -0,0 +1,31 @@
+package de.rocovomo.robot.leJOS.nxj.impl;
+
+import de.rocovomo.robot.api.RobotMovement;
+
+public class RobotMovementImpl implements RobotMovement {
+
+	@Override
+	public void moveForward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveBackward() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnLeft() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnRight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

d9fd4cde135ce4dabfe2bbc2b8fb955d4dbdfd9a
Volker W <vowe91@googlemail.com>
2013-05-02 20:20:11 +0200

    utility class Maps updated

--- de.rocovomo.util/src/de/rocovomo/util/Maps.java
+++ de.rocovomo.util/src/de/rocovomo/util/Maps.java
@@ -1,6 +1,7 @@
 package de.rocovomo.util;
 
 import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -8,6 +9,17 @@ import java.util.Set;
 public class Maps {
 
 	public static <K, V> Dictionary<K, V> toDictionary(Map<K, V> source, Dictionary<K, V> destination) {
+		Set<K> keys = source.keySet();
+		Iterator<K> keyIterator = keys.iterator();
+		while (keyIterator.hasNext()) {
+			K key = keyIterator.next();
+			destination.put(key, source.get(key));
+		}
+		return destination;
+	}
+	
+	public static <K,V> Dictionary<K, V> toDictionary(Map<K, V> source){
+		Dictionary<K, V> destination = new Hashtable<>();
 		Set<K> keys = source.keySet();
 		Iterator<K> keyIterator = keys.iterator();
 		while (keyIterator.hasNext()) {
